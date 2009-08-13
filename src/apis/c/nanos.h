d58f828bbf1f764d746740b0d24e1f29975ca6f9
Alex Duran <alex.duran@bsc.es>
2009-08-13 15:04:11 +0200

    add num_devices to create_and_run

--- src/apis/c/nanos.h
+++ src/apis/c/nanos.h
@@ -74,8 +74,8 @@ nanos_err_t nanos_create_wd ( nanos_wd_t *wd, size_t num_devices, nanos_device_t
 
 nanos_err_t nanos_submit ( nanos_wd_t wd, nanos_dependence_t *deps, nanos_team_t team );
 
-nanos_err_t nanos_create_wd_and_run ( nanos_device_t *devices, void * data, nanos_dependence_t *deps,
-                                      nanos_wd_props_t *props );
+nanos_err_t nanos_create_wd_and_run ( size_t num_devices, nanos_device_t *devices, void * data,
+                                      nanos_dependence_t *deps, nanos_wd_props_t *props );
 
 // Team related functions
 
