2e0b65b46ab0fa866855186afd860eb7366f9838
CARLO BERTOLLI <cbertoll@bscsmp01.bsc.es>
2009-08-26 12:54:02 +0200

    Solved task #66 Trottle is not applied if there are no properties

--- src/apis/c/nanos_wd.cpp
+++ src/apis/c/nanos_wd.cpp
@@ -25,7 +25,7 @@ nanos_err_t nanos_create_wd (  nanos_wd_t *uwd, size_t num_devices, nanos_device
                               void ** data, nanos_wg_t uwg, nanos_wd_props_t *props )
 {   
    try {
-      if ( props && !props->mandatory_creation && !sys.throttleTask() ) {
+      if ( ( (!props) ||  (props && !props->mandatory_creation) ) && !sys.throttleTask() ) {
          *uwd = 0; 
          return NANOS_OK;
       }
