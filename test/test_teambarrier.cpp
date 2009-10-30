0d2bbb9aaf9356da353906948a6966d8c3d7d12e
CARLO BERTOLLI <cbertoll@bscsmp01.bsc.es>
2009-10-30 15:22:34 +0100

    added test for team barrier

---
+++ test/test_teambarrier.cpp
@@ -0,0 +1,37 @@
+#include "config.hpp"
+#include "nanos.h"
+#include <iostream>
+#include "smpprocessor.hpp"
+#include "system.hpp"
+#include <string.h>
+
+using namespace std;
+using namespace nanos;
+
+#define BARR_NUM 10
+
+/*! works only with bf scheduler */
+void barrier_code ( void * )
+{
+       for ( int i = 0; i < BARR_NUM; i++ ) {
+              cout << "Before the barrier" << endl;
+              nanos_team_barrier();
+              cout << "After the barrier" << endl;
+
+       }
+}
+
+int main (int argc, char **argv)
+{
+       cout << "start" << endl;
+       //all threads perform a barrier: 
+       for ( int i = 1; i < sys.getNumPEs(); i++ ) {
+              WD * wd = new WD(new SMPDD(barrier_code));
+              sys.submit(*wd);
+       }
+       usleep(100);
+
+       barrier_code(NULL);
+
+       cout << "end" << endl;
+}
