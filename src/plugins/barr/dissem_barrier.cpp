4c7f91d04dbf233211f669a91f9cc3fa4ccc000d
Luis Martinell Andreu <luism@BSCIB76.bsc.es>
2009-12-22 10:30:21 +0100

    dissem_barrier partially ported to synchronizedcondition, not working yet.

--- src/plugins/barr/dissem_barrier.cpp
+++ src/plugins/barr/dissem_barrier.cpp
@@ -25,6 +25,7 @@
 #include "system.hpp"
 #include "atomic.hpp"
 #include "plugin.hpp"
+#include "synchronizedcondition.hpp"
 
 using namespace std;
 
@@ -43,6 +44,9 @@ namespace nanos {
             /*! the semaphores are implemented with a vector of atomic integers, because their number can change in successive
                invocations of the barrier method */
             vector<Atomic<int> > _semaphores;
+            vector<SingleSyncCond<int> *> _syncConds1;
+            vector<SingleSyncCond<int> *> _syncConds2;
+            Atomic<unsigned int> _ticket;
 
             /*! number of participants: can change dynamically */
             int _numParticipants;
@@ -70,6 +74,16 @@ namespace nanos {
          _numParticipants = numParticipants;
 
          _semaphores.resize( _numParticipants );
+         _syncConds1.resize( _numParticipants );
+         _syncConds2.resize( _numParticipants );
+
+         _ticket = 0;
+
+         for ( int i = 0; i < _numParticipants; i++ ) {
+            _semaphores[i] = 0;
+            _syncConds1[i] = new SingleSyncCond<int>( &_semaphores[i].override(), 0);
+            _syncConds2[i] = new SingleSyncCond<int>( &_semaphores[i].override(), -1);
+         }
 
          /*! we can compute the number of steps of the algorithm */
          _q = ( int ) ceil( log2( ( double ) _numParticipants ) );
@@ -82,6 +96,16 @@ namespace nanos {
          _numParticipants = numParticipants;
 
          _semaphores.resize( _numParticipants );
+         _syncConds1.resize( _numParticipants );
+         _syncConds2.resize( _numParticipants );
+
+         _ticket = 0;
+
+         for ( int i = 0; i < _numParticipants; i++ ) {
+            _semaphores[i] = 0;
+            _syncConds1[i] = new SingleSyncCond<int>( &_semaphores[i].override(), 0);
+            _syncConds2[i] = new SingleSyncCond<int>( &_semaphores[i].override(), -1);
+         }
 
          /*! we can re-compute the number of steps of the algorithm */
          _q = ( int ) ceil( log2( ( double ) _numParticipants ) );
@@ -90,24 +114,40 @@ namespace nanos {
 
       void DisseminationBarrier::barrier( int numParticipants )
       {
-         int myID = myThread->getId();
+//         int myID = myThread->getId();
+warning("got ticket " << _ticket.override());
+         int myID = (_ticket++)%_numParticipants;
 
          for ( int s = 0; s < _q; s++ ) {
             //compute the current step neighbour id
             int toSign = ( myID + ( int ) pow( 2,s ) ) % _numParticipants;
 
+warning(" waiting for "<< toSign << " value is " << _semaphores[toSign].override() << " expecting 0");
+            _syncConds1[toSign]->wait();
+            ( _semaphores[toSign] )--;
+warning(" signaling "<< toSign << " value set to " << _semaphores[toSign].override());
+            _syncConds2[toSign]->signal();
+
+warning(" 2 waiting for "<< myID << " value is " << _semaphores[myID].override() << " expecting -1");
+            _syncConds2[myID]->wait();
+            ( _semaphores[myID] )++;
+warning(" signaling "<< myID << " value set to " << _semaphores[myID].override());
+            _syncConds1[myID]->signal();
+/*
             //wait for the neighbour sem to reach the previous value
+warning(" waiting for "<< toSign << " sem value: "<< _semaphores[toSign].override());
             Scheduler::blockOnCondition( &_semaphores[toSign].override(), 0 );
             ( _semaphores[toSign] )--;
-
-            /*!
+            / *!
              *  Wait for the semaphore to be signaled for this round
              *  (check if it reached the number of step signals (+1 because the for starts from 0 
              *  for a correct computation of neighbours)
-             */
+             * /
+warning(" myself, for "<< myID << " sem value: "<< _semaphores[myID].override());
             Scheduler::blockOnCondition( &_semaphores[myID].override(), -1 );
 
             ( _semaphores[myID] )++;
+*/
          }
 
          /*! at the end of the protocol, we are guaranteed that the semaphores are all 0 */
