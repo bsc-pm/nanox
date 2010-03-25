ec612b6edb87f93b128541f4149156700c92c94c
Luis Martinell Andreu <luism@BSCIB76.bsc.es>
2010-03-25 11:17:55 +0100

    testinfo script was incorrect. use 'declare' instead of 'let'.

--- test/benchmkarks/test_nanox_creation.cpp
+++ test/benchmkarks/test_nanox_creation.cpp
@@ -4,7 +4,7 @@ test_generator=gens/nanox-creation-generator
 exec_versions="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24"
 for ever in $exec_versions;
 do
-  let test_ENV_$ever="NX_PES='$ever'"
+  declare test_ENV_$ever="NX_PES='$ever'"
 done
 </testinfo>
 */
