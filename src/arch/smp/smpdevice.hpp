e08f2b8fcfc14c60d10f264355123d18aa1497d1
Luis Martinell Andreu <luism@BSCIB76.bsc.es>
2010-04-15 16:01:37 +0200

    SMPMemory class moved to SMPDevice. Some more code documented.

--- src/arch/smp/smpdevice.hpp
+++ src/arch/smp/smpdevice.hpp
@@ -17,38 +17,65 @@
 /*      along with NANOS++.  If not, see <http://www.gnu.org/licenses/>.             */
 /*************************************************************************************/
 
-#ifndef _SMP_COPIER
-#define _SMP_COPIER
+#ifndef _SMP_DEVICE
+#define _SMP_DEVICE
 
 #include <stdint.h>
+#include <string.h>
+#include "workdescriptor_decl.hpp"
 
 namespace nanos
 {
 
-   class SMPMemory
+  /* \brief Device specialization for SMP architecture
+   * provides functions to allocate and copy data in the device
+   */
+   class SMPDevice : public Device
    {
       public:
-         void * allocate( size_t size )
+         /*! \brief SMPDevice constructor
+          */
+         SMPDevice ( const char *n ) : Device ( n ) {}
+
+         /*! \brief SMPDevice copy constructor
+          */
+         SMPDevice ( const SMPDevice &arch ) : Device ( arch ) {}
+
+         /*! \brief SMPDevice destructor
+          */
+         ~SMPDevice() {};
+
+        /* \breif allocate size bytes in the device
+         */
+         static void * allocate( size_t size )
          {
             return new char[size]; 
          }
 
-         void free( void *address )
+        /* \brief free address
+         */
+         static void free( void *address )
          {
             delete[] (char *) address;
          }
 
-         void copyIn( void *localDst, uint64_t remoteSrc, size_t size )
+        /* \brief copy from remoteSrc in the host to localDst in the device
+         */
+         static void copyIn( void *localDst, uint64_t remoteSrc, size_t size )
          {
             memcpy( localDst, (void *)remoteSrc, size );
          }
 
-         void copyOut( uint64_t remoteDst, void *localSrc, size_t size )
+        /* \brief copy from localSrc in the device to remoteDst in the host
+         */
+         static void copyOut( uint64_t remoteDst, void *localSrc, size_t size )
          {
             memcpy( (void *)remoteDst, localSrc, size );
          }
 
-         void copyLocal( void *dst, void *src, size_t size )
+        /* \brief copy localy in the device from src to dst
+         */
+         static void copyLocal( void *dst, void *src, size_t size )
          {
             memcpy( dst, src, size );
          }
