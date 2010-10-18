6d0a46dded5481772da83468983843a49dccdbef
Alex Duran <alex.duran@bsc.es>
2010-10-18 13:37:40 +0200

    Add missed include

---
+++ src/core/pminterface_decl.hpp
@@ -0,0 +1,40 @@
+/*************************************************************************************/
+/*      Copyright 2010 Barcelona Supercomputing Center                               */
+/*      Copyright 2009 Barcelona Supercomputing Center                               */
+/*                                                                                   */
+/*      This file is part of the NANOS++ library.                                    */
+/*                                                                                   */
+/*      NANOS++ is free software: you can redistribute it and/or modify              */
+/*      it under the terms of the GNU Lesser General Public License as published by  */
+/*      the Free Software Foundation, either version 3 of the License, or            */
+/*      (at your option) any later version.                                          */
+/*                                                                                   */
+/*      NANOS++ is distributed in the hope that it will be useful,                   */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU Lesser General Public License for more details.                          */
+/*                                                                                   */
+/*      You should have received a copy of the GNU Lesser General Public License     */
+/*      along with NANOS++.  If not, see <http://www.gnu.org/licenses/>.             */
+/*************************************************************************************/
+
+#ifndef NANOS_PM_INTERFACE
+#define NANOS_PM_INTERFACE
+
+#include "config.hpp"
+
+class PMInterface
+{
+   public:
+      virtual int getInternalDataSize() const { return 0; }
+
+      virtual void config (Config &cfg) {}
+      virtual void start () {}
+      virtual void finish() {}
+
+      virtual void setupWD( WD &wd ) {}
+      virtual void wdStarted( WD &wd ) {}
+      virtual void wdFinished( WD &wd ) {}
+};
+
+#endif /* PM_INTERFACE_HPP_ */
