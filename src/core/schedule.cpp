5dc298f24ad8d968fbac743f5c1f3b88790fa91d
Luis Martinell Andreu <luism@BSCIB76.bsc.es>
2010-11-10 21:06:29 +0100

    Added missing semicolon in instrumentation call.

--- src/core/schedule.cpp
+++ src/core/schedule.cpp
@@ -207,7 +207,7 @@ void Scheduler::waitOnCondition (GenericSyncCond *condition)
                NANOS_INSTRUMENT ( Values[2] = (nanos_event_value_t) time_yields; )
                NANOS_INSTRUMENT( sys.getInstrumentation()->raisePointEventNkvs(3, Keys, Values); )
 
-               NANOS_INSTRUMENT( InstrumentState inst2(NANOS_RUNTIME) );
+               NANOS_INSTRUMENT( InstrumentState inst2(NANOS_RUNTIME); );
                switchTo ( next );
                thread = getMyThreadSafe();
                NANOS_INSTRUMENT( inst2.close() );
