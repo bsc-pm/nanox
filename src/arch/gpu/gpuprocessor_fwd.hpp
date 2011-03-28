fa7cd5fa2170227780a6d9c7e212c52fa42a8a28
Judit Planas <judit.planas@bsc.es>
2011-03-28 11:23:10 +0200

    Add forward declaration for GPUProcessor class.

--- src/arch/gpu/gpuprocessor_fwd.hpp
+++ src/arch/gpu/gpuprocessor_fwd.hpp
@@ -17,60 +17,13 @@
 /*      along with NANOS++.  If not, see <http://www.gnu.org/licenses/>.             */
 /*************************************************************************************/
 
-#ifndef _NANOS_GPU_THREAD
-#define _NANOS_GPU_THREAD
-
-#include "compatibility.hpp"
-#include "gpudd.hpp"
-#include "gpuprocessor_fwd.hpp"
-#include "smpthread.hpp"
-
+#ifndef _NANOS_GPU_PROCESSOR_FWD
+#define _NANOS_GPU_PROCESSOR_FWD
 
 namespace nanos {
 namespace ext
 {
-
-   class GPUThread : public SMPThread
-   {
-      private:
-         int                        _gpuDevice; // Assigned GPU device Id
-         bool                       _wdClosingEvents; //! controls whether an instrumentation event should be generated at WD completion
-
-         // disable copy constructor and assignment operator
-         GPUThread( const GPUThread &th );
-         const GPUThread & operator= ( const GPUThread &th );
-
-         void raiseWDClosingEvents ();
-
-      public:
-         // constructor
-         GPUThread( WD &w, PE *pe, int device ) : SMPThread( w, pe ), _gpuDevice( device ),
-               _wdClosingEvents( false ) {}
-
-         // destructor
-         ~GPUThread() {}
-
-         void initializeDependent( void );
-         void runDependent ( void );
-
-         void inlineWorkDependent( WD &work );
-
-         void yield();
-
-         void idle();
-
-         int getGpuDevice ()
-         {
-            return _gpuDevice;
-         }
-
-         void enableWDClosingEvents ()
-         {
-            _wdClosingEvents = true;
-         }
-   };
-
-
+   class GPUProcessor;
 }
 }
 
