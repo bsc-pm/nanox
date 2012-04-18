7f4bcd0cd371fbc337ad0cd13b67e1ef1267643d
Guillermo Miranda Alamo <guillermo.miranda@bsc.es>
2012-04-18 12:29:05 +0200

    Added services to set delayed start, start nanos, and finish nanos

--- src/apis/c/nanos.h
+++ src/apis/c/nanos.h
@@ -168,6 +168,9 @@ NANOS_API_DECL(nanos_err_t, nanos_stop_scheduler, ());
 NANOS_API_DECL(nanos_err_t, nanos_scheduler_enabled, ( bool *res ));
 NANOS_API_DECL(nanos_err_t, nanos_wait_until_threads_paused, () );
 NANOS_API_DECL(nanos_err_t, nanos_wait_until_threads_unpaused, () );
+NANOS_API_DECL(nanos_err_t, nanos_delay_start, ());
+NANOS_API_DECL(nanos_err_t, nanos_start, ());
+NANOS_API_DECL(nanos_err_t, nanos_finish, ());
 
 // Memory management
 NANOS_API_DECL(nanos_err_t, nanos_malloc, ( void **p, size_t size, const char *file, int line ));
