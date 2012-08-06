50628b6883393fca6709bc0a08d58d8c3bc4eab9
Guillermo Miranda <guillermo.miranda@bsc.es>
2012-08-06 13:11:05 +0200

    · Removed unused System::checkConfig() · System has an optional list of valid plugins that will be populated by nanox --help. It will use that list if available. Closes #675.

--- src/support/config_decl.hpp
+++ src/support/config_decl.hpp
@@ -362,6 +362,23 @@ namespace nanos
                virtual MapVar * clone ();
          };
 
+         class PluginVar : public MapVar<std::string>
+         {
+            public:
+               //constructors
+               PluginVar( std::string &ref, const char* validList[], size_t validListCount ) :
+                     MapVar( ref )
+               {
+                  for ( size_t i = 0; i < validListCount; ++i )
+                     addOption( validList[i] );
+               }
+
+               // copy constructor
+               PluginVar( const PluginVar &opt ) : MapVar( opt ) {}
+               
+               PluginVar & addOption ( const std::string & value );
+         };
+
          class ActionFlag : public Option
          {
             public:
@@ -663,6 +680,7 @@ namespace nanos
         /* \brief initializes the config object
          */
          void init();
+         
 
         /* \brief Sets the current section in which new ConfigOptions will be listed
          * \param sectionName name of the section to be set as current
