ea8ed6a92cb51025af8bd7103b1d1b3c5f7c63e2
Xavier Teruel <xavier.teruel@bsc.es>
2013-03-01 18:53:45 +0100

    Selecting in between several devices when no versioning scheduler is used: closes #784

--- src/arch/gpu/gputhread.cpp
+++ src/arch/gpu/gputhread.cpp
@@ -109,7 +109,6 @@ void GPUThread::runDependent ()
 
 bool GPUThread::inlineWorkDependent ( WD &wd )
 {
-   GPUDD &dd = ( GPUDD & )wd.getActiveDevice();
    GPUProcessor &myGPU = * ( GPUProcessor * ) myThread->runningOn();
 
    if ( GPUConfig::isOverlappingInputsDefined() ) {
@@ -127,6 +126,8 @@ bool GPUThread::inlineWorkDependent ( WD &wd )
 
    // We wait for wd inputs, but as we have just waited for them, we could skip this step
    wd.start( WD::IsNotAUserLevelThread );
+
+   GPUDD &dd = ( GPUDD & ) wd.getActiveDevice();
 
    NANOS_INSTRUMENT ( InstrumentStateAndBurst inst1( "user-code", wd.getId(), NANOS_RUNNING ) );
    ( dd.getWorkFct() )( wd.getData() );
