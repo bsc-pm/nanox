bsc-pm/nanox

commit 9d72d14b6a7f2f19542fd6ca052767152f63fc99

Fixes problems with the order of NUMA information reading, PE creation and module creation (fixes #804, fixes #806)

src/core/system.cpp
@@ -352,8 +352,6 @@ void System::start ()
    if ( isHwlocAvailable() )
       loadHwloc();
 
-   loadModules();
-
    _targetThreads = _numThreads;
    // Do the same for the architecture plugins
    for ( ArchitecturePlugins::const_iterator it = _archs.begin();
@@ -362,6 +360,12 @@ void System::start ()
       _targetThreads += (*it)->getNumThreads();
    }
    
+   // Load & check NUMA config (depends on _targetThreads being already set)
+   loadNUMAInfo();
+
+   // Modules can be loaded now
+   loadModules();
+
    // Instrumentation startup
    NANOS_INSTRUMENT ( sys.getInstrumentation()->filterEvents( _instrumentDefault, _enableEvents, _disableEvents ) );
    NANOS_INSTRUMENT ( sys.getInstrumentation()->initialize() );
@@ -395,9 +399,6 @@ void System::start ()
 
    NANOS_INSTRUMENT ( sys.getInstrumentation()->raiseOpenStateEvent (NANOS_STARTUP) );
    
-   // Load & check NUMA config
-   loadNUMAInfo();
-
    // start of new PE/Worker creation
    // How many available physical PEs
    unsigned physPes = getCpuCount();

commit 3991df4a05bb1dce5a431549e365506ba9158d2e

Fixed a bug introduced in [9d72d14b6a7f2f19542fd6ca052767152f63fc99] that caused the System initialisation not to wait for the creation of CL and GPU threads

src/core/system.cpp
@@ -352,6 +352,13 @@ void System::start ()
    if ( isHwlocAvailable() )
       loadHwloc();
 
+   
+   // Load & check NUMA config
+   loadNUMAInfo();
+
+   // Modules can be loaded now
+   loadModules();
+
    _targetThreads = _numThreads;
    // Do the same for the architecture plugins
    for ( ArchitecturePlugins::const_iterator it = _archs.begin();
@@ -359,12 +366,9 @@ void System::start ()
    {
       _targetThreads += (*it)->getNumThreads();
    }
-   
-   // Load & check NUMA config (depends on _targetThreads being already set)
-   loadNUMAInfo();
 
-   // Modules can be loaded now
-   loadModules();
+   // Check if the NUMA and other arguments make sense (depends on _targetThreads)
+   checkArguments();
 
    // Instrumentation startup
    NANOS_INSTRUMENT ( sys.getInstrumentation()->filterEvents( _instrumentDefault, _enableEvents, _disableEvents ) );

src/core/system.hpp
@@ -192,6 +192,10 @@ inline void System::loadNUMAInfo ()
    if ( _coresPerSocket == 0 )
       _coresPerSocket = std::ceil( _targetThreads / static_cast<float>( _numSockets ) );
 #endif
+}
+
+inline void System::checkArguments()
+{
    // Check NUMA config
    if ( _numSockets != std::ceil( _targetThreads / static_cast<float>( _coresPerSocket ) ) )
    {

src/core/system_decl.hpp
@@ -351,6 +351,13 @@ namespace nanos
           * Uses hwloc if available, and also checks if both settings make sense.
           */
          void loadNUMAInfo ();
+
+         /**
+          * \brief Verifies that NUMA-related arguments (and others, possibly)
+          * make sense, such as the number of cores per node, number of nodes,
+          * and number of threads.
+          */
+         void checkArguments ();
          
          /** \brief Retrieves the NUMA node of a given PE.
           *  \note Will use hwloc if available.