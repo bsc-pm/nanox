5410a23b6b33c094b5047c1aaa612a225c329712
Victor Lopez <victor.lopez@bsc.es>
2013-07-25 16:06:25 +0200

    Add PMInterface class to nanos namespace

--- src/core/pminterface_decl.hpp
+++ src/core/pminterface_decl.hpp
@@ -25,48 +25,51 @@
 #include "workdescriptor_decl.hpp"
 #include "threadteam_decl.hpp"
 
-class PMInterface
+namespace nanos
 {
-   protected:
-      std::string    _description; /*!< \brief String describing Programming Model Interface */
-      bool           _malleable;   /*!< \brief Can the PM dynamically change the team size? */
-   private:
-      /*! \brief PMInterface copy constructor (private)
-       */
-      PMInterface( PMInterface &pmi );
-      /*! \brief PMInterface copy assignment operator (private)
-       */
-      PMInterface& operator= ( PMInterface &pmi );
-   public:
-      /*! \brief PMInterface default constructor
-       */
-      PMInterface() {}
-      /*! \brief PMInterface destructor
-       */
-      virtual ~PMInterface() {}
+   class PMInterface
+   {
+      protected:
+         std::string    _description; /*!< \brief String describing Programming Model Interface */
+         bool           _malleable;   /*!< \brief Can the PM dynamically change the team size? */
+      private:
+         /*! \brief PMInterface copy constructor (private)
+          */
+         PMInterface( PMInterface &pmi );
+         /*! \brief PMInterface copy assignment operator (private)
+          */
+         PMInterface& operator= ( PMInterface &pmi );
+      public:
+         /*! \brief PMInterface default constructor
+          */
+         PMInterface() {}
+         /*! \brief PMInterface destructor
+          */
+         virtual ~PMInterface() {}
 
-      virtual int getInternalDataSize() const { return 0; }
-      virtual int getInternalDataAlignment() const { return 1; }
-      virtual void initInternalData( void *data ) {}
+         virtual int getInternalDataSize() const { return 0; }
+         virtual int getInternalDataAlignment() const { return 1; }
+         virtual void initInternalData( void *data ) {}
 
-      virtual void config (nanos::Config &cfg) {}
-      virtual void start () { _description = std::string("none"); }
-      virtual void finish() {}
+         virtual void config (nanos::Config &cfg) {}
+         virtual void start () { _description = std::string("none"); }
+         virtual void finish() {}
 
-      virtual void setupWD( nanos::WD &wd ) {}
-      virtual void wdStarted( nanos::WD &wd ) {}
-      virtual void wdFinished( nanos::WD &wd ) {}
+         virtual void setupWD( nanos::WD &wd ) {}
+         virtual void wdStarted( nanos::WD &wd ) {}
+         virtual void wdFinished( nanos::WD &wd ) {}
 
-      virtual nanos::ThreadTeamData* getThreadTeamData() { return NEW nanos::ThreadTeamData(); }
-      std::string getDescription( void ) { return _description; }
+         virtual nanos::ThreadTeamData* getThreadTeamData() { return NEW nanos::ThreadTeamData(); }
+         std::string getDescription( void ) { return _description; }
 
-      bool isMalleable( void ) const { return _malleable; }
+         bool isMalleable( void ) const { return _malleable; }
 
-      virtual void setNumThreads( int nthreads ) {}
+         virtual void setNumThreads( int nthreads ) {}
 
-      virtual void getCpuMask( cpu_set_t *cpu_set ) {}
-      virtual void setCpuMask( const cpu_set_t *cpu_set ) {}
-      virtual void addCpuMask( const cpu_set_t *cpu_set ) {}
-};
+         virtual void getCpuMask( cpu_set_t *cpu_set ) {}
+         virtual void setCpuMask( const cpu_set_t *cpu_set ) {}
+         virtual void addCpuMask( const cpu_set_t *cpu_set ) {}
+   };
+}
 
 #endif /* PM_INTERFACE_HPP_ */
