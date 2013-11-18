c21b02ab1cded3c6abc73f0655251e1fa46a2968
Xavier Teruel <xavier.teruel@bsc.es>
2013-11-18 12:19:50 +0100

    Removing comment

--- src/core/system.cpp
+++ src/core/system.cpp
@@ -1491,7 +1491,6 @@ ThreadTeam * System::createTeam ( unsigned nthreads, void *constraints, bool reu
 void System::endTeam ( ThreadTeam *team )
 {
    debug("Destroying thread team " << team << " with size " << team->size() );
-/*** Marta ***/
 
    dlb_returnCpusIfNeeded();
    while ( team->size ( ) > 0 ) {
