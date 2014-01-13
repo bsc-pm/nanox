ca48e302eea9ea25167850092db1b13053fd3a26
Guillermo Miranda <guillermo.miranda@bsc.es>
2014-01-13 13:30:36 +0100

    DependableObject::getId() is now const. Added a const version of DependableObject::getRelatedObject()

--- src/core/dependableobject_decl.hpp
+++ src/core/dependableobject_decl.hpp
@@ -131,6 +131,11 @@ namespace nanos
          /*! \brief Get the related object which actually has the dependence
           */
          virtual void * getRelatedObject ( );
+         
+         /*! \brief Get the related object which actually has the dependence
+          * (const version)
+          */
+         virtual const void * getRelatedObject ( ) const;
 
          /*! \brief Instrument predecessor -> successor dependency
           */
@@ -145,7 +150,7 @@ namespace nanos
         /*! \brief Id getter function.
          *         Returns the id  for the DependableObject (unique in its domain).
          */
-         unsigned int getId ();
+         unsigned int getId () const;
 
         /*! \brief Increase the number of predecessors of the DependableObject.
          */
