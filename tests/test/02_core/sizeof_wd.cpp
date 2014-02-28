98398c199df96b9c19a8531e79fd63c5d4458645
Xavier Teruel <xavier.teruel@bsc.es>
2014-02-28 16:04:19 +0100

    Adding test controlling size of WorkDescriptor; see #930

---
+++ tests/test/02_core/sizeof_wd.cpp
@@ -0,0 +1,40 @@
+/*************************************************************************************/
+/*      Copyright 2009 Barcelona Supercomputing Center                               */
+/*                                                                                   */
+/*      This file is part of the NANOS++ library.                                    */
+/*                                                                                   */
+/*      NANOS++ is free software: you can redistribute it and/or modify              */
+/*      it under the terms of the GNU Lesser General Public License as published by  */
+/*      the Free Software Foundation, either version 3 of the License, or            */
+/*      (at your option) any later version.                                          */
+/*                                                                                   */
+/*      NANOS++ is distributed in the hope that it will be useful,                   */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU Lesser General Public License for more details.                          */
+/*                                                                                   */
+/*      You should have received a copy of the GNU Lesser General Public License     */
+/*      along with NANOS++.  If not, see <http://www.gnu.org/licenses/>.             */
+/*************************************************************************************/
+
+/*
+<testinfo>
+test_generator=gens/core-generator
+test_schedule="bf"
+test_max_cpus=1
+</testinfo>
+*/
+
+#include "system.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace nanos;
+
+int main ( int argc, char **argv )
+{
+   int error = 0;
+   cout << "Size of WorkDescriptor class: " << sizeof(WD) << endl;
+   if ( sizeof(WD) > 640 ) error = 1;
+   return error;
+}
