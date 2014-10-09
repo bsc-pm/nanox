e4046598309228c31597bc39d2795a123dbe128f
Judit Planas <judit.planas@bsc.es>
2014-10-09 14:14:07 +0200

    Adapted DO::decreasePredecessors() to fit the current definition.

--- src/plugins/deps/cregions_deps_nocache.cpp
+++ src/plugins/deps/cregions_deps_nocache.cpp
@@ -241,7 +241,7 @@ namespace nanos {
                depObj.submitted();
             
                // now everything is ready
-               depObj.decreasePredecessors( &flushDeps, true );
+               depObj.decreasePredecessors( &flushDeps, NULL, false, true );
             }
             /*! \brief Adds a region access of a DependableObject to the domains dependency system.
              *  \param depObj target DependableObject
