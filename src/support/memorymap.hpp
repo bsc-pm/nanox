91ddb7f899acf4c99873c4cd6e3f5213dcabf133
Javier Bueno Hedo <javier.bueno@bsc.es>
2014-11-19 11:10:27 +0100

    Bug in MemoryMap

--- src/support/memorymap.hpp
+++ src/support/memorymap.hpp
@@ -1622,9 +1622,21 @@ _Type **MemoryMap< _Type >::getExactOrFullyOverlappingInsertIfNotFound( uint64_t
    } else if ( this->key_comp()( key, it->first ) ) {
       /* key less than it->first, not total overlap guaranteed */
       if ( it->first.checkOverlap( key ) == MemoryChunk::NO_OVERLAP ) {
-         it = this->insert( it, typename BaseMap::value_type( key, ( _Type * ) NULL ) );
-         ptr = &( it->second );
-         exact = true;
+         it--;
+         MemoryChunk::OverlapType ov = it->first.checkOverlap( key );
+         if ( ov == MemoryChunk::NO_OVERLAP ) {
+            it = this->insert( it, typename BaseMap::value_type( key, ( _Type * ) NULL ) );
+            ptr = &( it->second );
+            exact = true;
+         } else if ( ov == MemoryChunk::SUBCHUNK_OVERLAP ||
+               ov == MemoryChunk::SUBCHUNK_BEGIN_OVERLAP ||
+               ov == MemoryChunk::SUBCHUNK_END_OVERLAP) {
+            ptr = &( it->second );
+            exact = false;
+         }
+         //it = this->insert( it, typename BaseMap::value_type( key, ( _Type * ) NULL ) );
+         //ptr = &( it->second );
+         //exact = true;
       } else {
          ptr = NULL;
          exact = false;
