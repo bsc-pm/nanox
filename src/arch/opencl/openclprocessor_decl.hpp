3baf977f2e12493562a9c267fd76336c82c93fde
Diego Nieto <diego.nieto@bsc.es>
2015-03-17 15:46:34 +0100

    Moved define for deprecated methods to better place

--- src/arch/opencl/openclprocessor_decl.hpp
+++ src/arch/opencl/openclprocessor_decl.hpp
@@ -20,7 +20,6 @@
 #ifndef _NANOS_OpenCL_PROCESSOR_DECL
 #define _NANOS_OpenCL_PROCESSOR_DECL
 
-#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 #include "cachedaccelerator.hpp"
 #include "openclcache.hpp"
 #include "openclconfig.hpp"
