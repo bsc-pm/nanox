6e61b619f541a9ac754264c8ababaf19de255e5c
Jorge Bellon <jbellon@bsc.es>
2015-04-16 11:57:44 +0200

    Split atomic.hpp into multiple header files.

--- src/support/atomic_decl.hpp
+++ src/support/atomic_decl.hpp
@@ -20,10 +20,6 @@
 #ifndef _NANOS_ATOMIC_DECL
 #define _NANOS_ATOMIC_DECL
 
-#include "compatibility.hpp"
-#include "nanos-int.h"
-#include <algorithm> // for min/max
-#include <cstddef>
 
 /* TODO: move to configure
 #include <ext/atomicity.h>
@@ -31,18 +27,6 @@
 #error "Atomic gcc builtins support is mandatory at this point"
 #endif
 */
-
-namespace nanos
-{
-   /** \brief Forwared declaration required by RecursiveLock */
-   class BaseThread;
-}
-
-namespace nanos
-{
-   class BaseThread;
-}
-
 
 namespace nanos
 {
@@ -125,141 +109,6 @@ namespace nanos
    bool compareAndSwap( volatile T *ptr, T oldval, T  newval );
 #endif
 
-   class Lock : public nanos_lock_t
-   {
-      private:
-         typedef nanos_lock_state_t state_t;
-
-         // disable copy constructor and assignment operator
-         Lock( const Lock &lock );
-         const Lock & operator= ( const Lock& );
-
-      public:
-         // constructor
-         Lock( state_t init=NANOS_LOCK_FREE ) : nanos_lock_t( init ) {};
-
-         // destructor
-         ~Lock() {}
-
-         void acquire ( void );
-         void acquire_noinst ( void );
-         bool tryAcquire ( void );
-         void release ( void );
-
-         state_t operator* () const;
-
-         state_t getState () const;
-
-         void operator++ ( int val );
-
-         void operator-- ( int val );
-   };
-
-   class LockBlock
-   {
-     private:
-       Lock & _lock;
-
-       // disable copy-constructor
-       explicit LockBlock ( const LockBlock & );
-
-     public:
-       LockBlock ( Lock & lock );
-       ~LockBlock ( );
-
-       void acquire();
-       void release();
-   };
-
-   class LockBlock_noinst
-   {
-     private:
-       Lock & _lock;
-
-       // disable copy-constructor
-       explicit LockBlock_noinst ( const LockBlock_noinst & );
-
-     public:
-       LockBlock_noinst ( Lock & lock );
-       ~LockBlock_noinst ( );
-
-       void acquire();
-       void release();
-   };
-
-   class SyncLockBlock : public LockBlock
-   {
-     private:
-       // disable copy-constructor
-       explicit SyncLockBlock ( const SyncLockBlock & );
-
-     public:
-       SyncLockBlock ( Lock & lock );
-       ~SyncLockBlock ( );
-   };
-
-   class RecursiveLock : public nanos_lock_t
-   {
-
-      private:
-         BaseThread *_holderThread;
-         std::size_t _recursionCount;
-         
-         typedef nanos_lock_state_t state_t;
-
-         // disable copy constructor and assignment operator
-         RecursiveLock( const RecursiveLock &lock );
-         const RecursiveLock & operator= ( const RecursiveLock& );
-
-      public:
-         // constructor
-         RecursiveLock( state_t init=NANOS_LOCK_FREE )
-            : nanos_lock_t( init ), _holderThread( 0 ), _recursionCount( 0UL )
-         {};
-
-         // destructor
-         ~RecursiveLock() {}
-
-         void acquire ( void );
-         bool tryAcquire ( void );
-         void release ( void );
-
-         state_t operator* () const;
-
-         state_t getState () const;
-
-         void operator++ ( int );
-
-         void operator-- ( int );
-   };
-
-   class RecursiveLockBlock
-   {
-     private:
-       RecursiveLock & _lock;
-
-       // disable copy-constructor
-       explicit RecursiveLockBlock ( const RecursiveLock & );
-
-     public:
-       RecursiveLockBlock ( RecursiveLock & lock );
-       ~RecursiveLockBlock ( );
-
-       void acquire();
-       void release();
-   };
-
-   class SyncRecursiveLockBlock : public RecursiveLockBlock
-   {
-     private:
-       // disable copy-constructor
-       explicit SyncRecursiveLockBlock ( const SyncRecursiveLockBlock & );
-
-     public:
-       SyncRecursiveLockBlock ( RecursiveLock & lock );
-       ~SyncRecursiveLockBlock ( );
-   };
-
-};
+}
 
 #endif
