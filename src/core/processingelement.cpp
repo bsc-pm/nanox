8b2b0b45477b32838d73ea50af36d171dd0d2a35
Xavier Teruel <xavier.teruel@bsc.es>
2015-05-29 12:30:53 +0200

    Workaround to fix admit current thread problem; also creating a ticket to decide whether to change vector for a list or discontinue admit current thread services

--- src/core/processingelement.cpp
+++ src/core/processingelement.cpp
@@ -33,16 +33,21 @@ using namespace nanos;
 ProcessingElement::ProcessingElement ( const Device *arch, unsigned int memSpaceId,
    unsigned int clusterNode, unsigned int numaNode, bool inNumaNode, unsigned int socket, bool inSocket ) : 
    Location( clusterNode, numaNode, inNumaNode, socket, inSocket ), 
-   _id ( sys.nextPEId() ), _supportedDevices( 1, arch ), _device ( arch ), _threads(), _memorySpaceId( memSpaceId ) {}
+   _id ( sys.nextPEId() ), _supportedDevices( 1, arch ), _device ( arch ), _threads(), _memorySpaceId( memSpaceId )
+{
+   _threads.reserve(1024);
+}
 
 ProcessingElement::ProcessingElement ( const Device **archs, unsigned int numArchs, unsigned int memSpaceId,
    unsigned int clusterNode, unsigned int numaNode, bool inNumaNode, unsigned int socket, bool inSocket ) : 
    Location( clusterNode, numaNode, inNumaNode, socket, inSocket ), 
-   _id ( sys.nextPEId() ), _supportedDevices( numArchs, NULL ), _device ( archs[0] ), _threads(), _memorySpaceId( memSpaceId ) {
-      for(unsigned int idx = 0; idx < numArchs; idx += 1) {
-         _supportedDevices[idx] = archs[idx];
-      }
+   _id ( sys.nextPEId() ), _supportedDevices( numArchs, NULL ), _device ( archs[0] ), _threads(), _memorySpaceId( memSpaceId ) 
+{
+   _threads.reserve(1024);
+   for(unsigned int idx = 0; idx < numArchs; idx += 1) {
+      _supportedDevices[idx] = archs[idx];
    }
+}
 
 void ProcessingElement::copyDataIn( WorkDescriptor &work )
 {
