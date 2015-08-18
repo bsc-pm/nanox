f8ef2e108a776890df8528fc583c57ef938828a2
Victor Lopez <victor.lopez@bsc.es>
2015-08-18 14:50:40 +0200

    Minor improvements in nanox-bindings

--- src/utils/nanox_bindings.cpp
+++ src/utils/nanox_bindings.cpp
@@ -17,8 +17,13 @@
 /*      along with NANOS++.  If not, see <http://www.gnu.org/licenses/>.             */
 /*************************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <iostream>
 #include <unistd.h>
+#include <getopt.h>
 #include "os.hpp"
 #include "system.hpp"
 #include "debug.hpp"
@@ -30,11 +35,74 @@
 
 using namespace nanos;
 
+static void print_version()
+{
+   std::cout << PACKAGE << " " << VERSION << " (" << NANOX_BUILD_VERSION << ")" <<  std::endl;
+   std::cout << "Configured with: " << NANOX_CONFIGURE_ARGS << std::endl;
+}
+
+static void print_help( const char* program )
+{
+   std::cout << "usage: " << program << " [-a|--all]" << std::endl;
+   std::cout << std::endl;
+   std::cout << "Report Nanos++ bindings according to the process affinity mask and the runtime workers" << std::endl;
+   std::cout << std::endl;
+   std::cout << "Options:" << std::endl;
+   std::cout << "  -a, --all:    report all available bindings, ignoring number of threads" << std::endl;
+   std::cout << "  -h, --help:   print this help" << std::endl;
+   std::cout << std::endl;
+   std::cout << "Examples:" << std::endl;
+   std::cout << "  > nanox-bindings" << std::endl;
+   std::cout << "  Nanos++: hostname::pid [ 0-15 ]" << std::endl;
+   std::cout << "  > NX_ARGS=\"--smp-workers=4 --binding-stride=2\" nanox-bindings" << std::endl;
+   std::cout << "  Nanos++: hostname::pid [ 0,2,4,6 ]" << std::endl;
+}
+
 int main( int argc, char *argv[] )
 {
-   int pid = getpid();
+   bool do_all = false;
+   bool do_help = false;
+   bool do_version = false;
 
-   const CpuSet& cpu_set = sys.getCpuProcessMask();
+   int opt;
+   struct option long_options[] = {
+      {"all",     no_argument, 0, 'a'},
+      {"help",    no_argument, 0, 'h'},
+      {"version", no_argument, 0, 'v'},
+      {0,         0,           0, 0 }
+   };
+
+   while ( (opt = getopt_long(argc, argv, "ahv", long_options, NULL)) != -1 ) {
+      switch (opt) {
+         case 'a':
+            do_all = true;
+            break;
+         case 'h':
+            do_help = true;
+            break;
+         case 'v':
+            do_version = true;
+            break;
+         default:
+            print_help( argv[0] );
+            exit( EXIT_SUCCESS );
+      }
+
+   }
+
+   if (do_version) {
+      print_version();
+      exit( EXIT_SUCCESS );
+   }
+
+   if (do_help) {
+      print_help( argv[0] );
+      exit( EXIT_SUCCESS );
+   }
+
+   pid_t pid = getpid();
+
+   const CpuSet& cpu_set = do_all ? sys.getCpuProcessMask() : sys.getCpuActiveMask();
 
    char hostname[HOST_NAME_MAX];
    gethostname( hostname, HOST_NAME_MAX );
@@ -42,5 +110,5 @@ int main( int argc, char *argv[] )
    std::cout << "Nanos++: " << hostname << "::" << pid
       << " [ " << cpu_set.toString() << " ]" << std::endl;
 
-   return 0;
+   exit( EXIT_SUCCESS );
 }
