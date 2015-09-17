9a8ef2a922acd0b7d5389883031560c09f50edd5
Diego Nieto <diego.nieto@bsc.es>
2015-09-17 18:07:39 +0200

    Changing the casting from size_t to uint64_t on OpenCL plugin
    Different ways to work with a void* on OpenCL are causing problems on the cache of buffers. See #1118

--- src/arch/opencl/openclprocessor.cpp
+++ src/arch/opencl/openclprocessor.cpp
@@ -221,12 +221,12 @@ void OpenCLAdapter::freeAddr(void* addrin )
    }
 }
 
-size_t OpenCLAdapter::getSizeFromCache(size_t addr){
+size_t OpenCLAdapter::getSizeFromCache(uint64_t addr){
     return _sizeCache[addr];
 }
 
 cl_mem OpenCLAdapter::getBuffer(SimpleAllocator& allocator, cl_mem parentBuf,
-                               size_t devAddr,
+                               uint64_t devAddr,
                                size_t size)
 {
    std::pair<uint64_t,size_t> cacheKey= std::make_pair(devAddr,size);
@@ -240,7 +240,7 @@ cl_mem OpenCLAdapter::getBuffer(SimpleAllocator& allocator, cl_mem parentBuf,
    //If there is a buffer which covers this buffer (same base address but bigger), return it
    uint64_t baseAddress;
    if (isSharedMem) {       
-      baseAddress=(size_t )OpenCLProcessor::getSharedMemAllocator().getBasePointer( (void*) devAddr, size) ;
+      baseAddress=reinterpret_cast<uint64_t>(OpenCLProcessor::getSharedMemAllocator().getBasePointer( (void*) devAddr, size)) ;
    } else {
       //If there is a buffer which covers this buffer (same base address but bigger), return it
       baseAddress=allocator.getBasePointer(devAddr, size);     
@@ -304,7 +304,7 @@ cl_mem OpenCLAdapter::getBuffer(SimpleAllocator& allocator, cl_mem parentBuf,
 
 
 cl_mem OpenCLAdapter::createBuffer(cl_mem parentBuf,
-                               size_t devAddr,
+                               uint64_t devAddr,
                                size_t size,
                                void* hostPtr)
 {
@@ -1393,7 +1393,7 @@ void* OpenCLProcessor::allocateSharedMemory( size_t size ){
 }
 
 void OpenCLProcessor::freeSharedMemory( void* addr ){    
-    _openclAdapter.freeSharedMemBuffer((void*)((size_t)addr));
+    _openclAdapter.freeSharedMemBuffer((void*)(addr));
 }
 
 BaseThread &OpenCLProcessor::startOpenCLThread() {
