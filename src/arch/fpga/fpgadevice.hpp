6a3a9551d0d80e8cad604a0f979f2172f89c6ff5
Antonio Filgueras <antonio.filgueras@bsc.es>
2015-11-19 12:10:48 +0100

    Fix some methods being wrongly defined as const in FPGADevice

--- src/arch/fpga/fpgadevice.hpp
+++ src/arch/fpga/fpgadevice.hpp
@@ -42,17 +42,17 @@ namespace nanos
                  WorkDescriptor const &wd, unsigned int copyIdx);
          virtual void memFree( uint64_t addr, SeparateMemoryAddressSpace &mem );
 
-         virtual void _canAllocate( SeparateMemoryAddressSpace const &mem, std::size_t *sizes,
-                 unsigned int numChunks, std::size_t *remainingSizes ) const {}
+         virtual void _canAllocate( SeparateMemoryAddressSpace &mem, std::size_t *sizes,
+                 unsigned int numChunks, std::size_t *remainingSizes ) {}
 
-         virtual std::size_t getMemCapacity( SeparateMemoryAddressSpace const &mem )const {
+         virtual std::size_t getMemCapacity( SeparateMemoryAddressSpace &mem ) {
             //return 1GB of memory available. There is no addressable memory on the device
             //Just return 1GB as this is the total system memmory.
             return 1024*1024*1024;
          }
          virtual void _copyIn( uint64_t devAddr, uint64_t hostAddr, std::size_t len,
                SeparateMemoryAddressSpace &mem, DeviceOps *ops, Functor *f,
-               WD const &wd, void *hostObject, reg_t hostRegionId ) const;
+               WD const &wd, void *hostObject, reg_t hostRegionId );
 
          /*! \brief Copy from remoteSrc in the host to localDst in the device
           *        Returns true if the operation is synchronous
@@ -61,7 +61,7 @@ namespace nanos
 
          virtual void _copyOut( uint64_t hostAddr, uint64_t devAddr, std::size_t len,
                SeparateMemoryAddressSpace &mem, DeviceOps *ops, Functor *f,
-               WorkDescriptor const &wd, void *hostObject, reg_t hostRegionId ) const;
+               WorkDescriptor const &wd, void *hostObject, reg_t hostRegionId );
          /*! \brief Copy from localSrc in the device to remoteDst in the host
           *        Returns true if the operation is synchronous
           */
@@ -85,14 +85,14 @@ namespace nanos
          virtual bool _copyDevToDev( uint64_t devDestAddr, uint64_t devOrigAddr, std::size_t len,
                SeparateMemoryAddressSpace &memDest, SeparateMemoryAddressSpace &memorig,
                DeviceOps *ops, Functor *f, WorkDescriptor const &wd, void *hostObject,
-               reg_t hostRegionId ) const { std::cerr << "wrong copyDevToDev" <<std::endl; return false; }
+               reg_t hostRegionId ) { std::cerr << "wrong copyDevToDev" <<std::endl; return false; }
 
-         virtual void _getFreeMemoryChunksList( SeparateMemoryAddressSpace const &mem,
-               SimpleAllocator::ChunkList &list ) const { std::cerr << "wrong _getFreeMemoryChunksList()" <<std::endl; }
+         virtual void _getFreeMemoryChunksList( SeparateMemoryAddressSpace &mem,
+               SimpleAllocator::ChunkList &list ) { std::cerr << "wrong _getFreeMemoryChunksList()" <<std::endl; }
 
          //not supported
          virtual void _copyInStrided1D( uint64_t devAddr, uint64_t hostAddr, std::size_t len,
-               std::size_t numChunks, std::size_t ld, SeparateMemoryAddressSpace const &mem,
+               std::size_t numChunks, std::size_t ld, SeparateMemoryAddressSpace &mem,
                DeviceOps *ops, Functor *f, WorkDescriptor const &wd, void *hostObject,
                reg_t hostRegionId )
          {
@@ -111,9 +111,9 @@ namespace nanos
          //not supported
          virtual bool _copyDevToDevStrided1D( uint64_t devDestAddr, uint64_t devOrigAddr,
                std::size_t len, std::size_t numChunks, std::size_t ld,
-               SeparateMemoryAddressSpace const &memDest, SeparateMemoryAddressSpace const &memOrig,
+               SeparateMemoryAddressSpace &memDest, SeparateMemoryAddressSpace &memOrig,
                DeviceOps *ops, Functor *f, WorkDescriptor const &wd, void *hostObject,
-               reg_t hostRegionId ) const
+               reg_t hostRegionId )
          {
 
             warning( "Strided fpga copies not implemented" );
