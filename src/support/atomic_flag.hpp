2d86d1822a2d63f682bc000c49751e51c43a3b3c
Jorge Bellon <jorge.bellon@bsc.es>
2016-03-03 16:27:06 +0100

    Adding an implementation of C++11 atomic_flag
    This implementation uses GCC builting atomic intrinsics already
    present in the regular Atomic class.

---
+++ src/support/atomic_flag.hpp
@@ -0,0 +1,94 @@
+/*************************************************************************************/
+/*      Copyright 2015 Barcelona Supercomputing Center                               */
+/*                                                                                   */
+/*      This file is part of the NANOS++ library.                                    */
+/*                                                                                   */
+/*      NANOS++ is free software: you can redistribute it and/or modify              */
+/*      it under the terms of the GNU Lesser General Public License as published by  */
+/*      the Free Software Foundation, either version 3 of the License, or            */
+/*      (at your option) any later version.                                          */
+/*                                                                                   */
+/*      NANOS++ is distributed in the hope that it will be useful,                   */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU Lesser General Public License for more details.                          */
+/*                                                                                   */
+/*      You should have received a copy of the GNU Lesser General Public License     */
+/*      along with NANOS++.  If not, see <http://www.gnu.org/licenses/>.             */
+/*************************************************************************************/
+
+#ifndef _NANOS_ATOMIC_FLAG
+#define _NANOS_ATOMIC_FLAG
+
+namespace nanos {
+
+/**
+ * \class atomic_flag is an atomic boolean type.
+ * Unlike all specializations of Atomic, it is guaranteed to be lock-free.
+ * Unlike Atomic<bool>, atomic_flag does not provide load or store operations.
+ * It is available in libstdc++ since C++11 standard.
+ * See http://en.cppreference.com/w/cpp/atomic/atomic_flag
+ */
+class atomic_flag {
+	private:
+#ifdef HAVE_NEW_GCC_ATOMIC_OPS
+		bool          _value;
+#else
+		volatile bool _value;
+#endif
+
+	public:
+		/**
+		 * Default constructor for atomic_flag
+		 */
+		atomic_flag() : _value(false) { clear(); }
+
+		/**
+		 * Destructor for class atomic_flag
+		 */
+		~atomic_flag() {}
+
+	private:
+		/**
+		 * Atomic flag is not copyable
+		 */
+		atomic_flag( const atomic_flag& );
+
+		/**
+		 * Atomic flag is not copyable
+		 */
+		atomic_flag& operator=( const atomic_flag& );
+
+		/**
+		 * Atomic flag is not copyable
+		 */
+		atomic_flag& operator=( const atomic_flag& ) volatile;
+
+	public:
+		bool test_and_set();
+
+		void clear();
+};
+
+bool atomic_flag::test_and_set()
+{
+#ifdef HAVE_NEW_GCC_ATOMIC_OPS
+	return __atomic_test_and_set(&_value,__ATOMIC_RELEASE);
+#else
+	return __sync_lock_test_and_set(&_value, true);
+#endif
+}
+
+void atomic_flag::clear()
+{
+#ifdef HAVE_NEW_GCC_ATOMIC_OPS
+	__atomic_clear(&_value,__ATOMIC_RELEASE);
+#else
+	__sync_lock_release(&_value);
+#endif
+}
+
+} // namespace nanos
+
+#endif // _NANOS_ATOMIC_FLAG
+
