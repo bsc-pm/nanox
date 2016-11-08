29369fc6083e0f6aa346ee97f3769d7809d68eaa
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2016-11-08 14:33:01 +0100

    Fixing a parameter of an overloaded  member function
    See #1192

--- src/plugins/sched/affinity_sched.cpp
+++ src/plugins/sched/affinity_sched.cpp
@@ -1398,7 +1398,7 @@ namespace nanos {
             virtual WD *atIdle ( BaseThread *thread, int numSteal );
             virtual WD *atBlock ( BaseThread *thread, WD *current );
 
-            virtual WD *atAfterExit ( BaseThread *thread, WD *current )
+            virtual WD *atAfterExit ( BaseThread *thread, WD *current, int numStealDummy )
             {
                return atBlock(thread, current );
             }
