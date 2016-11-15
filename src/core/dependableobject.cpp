668c4bbdab99cfb8ac2d62576781d55bf37746b5
Victor Lopez <victor.lopez@bsc.es>
2016-11-15 12:36:24 +0100

    Move addSuccessor back to the header file

--- src/core/dependableobject.cpp
+++ src/core/dependableobject.cpp
@@ -246,38 +246,3 @@ DependableObject * DependableObject::releaseImmediateSuccessor ( DependableObjec
    }
    return found;
 }
-bool DependableObject::addSuccessor ( DependableObject &depObj )
-{
-   // Avoiding create cycles in dependence graph
-   if ( this == &depObj ) return false;
-
-   if ( sys._preSchedule ) {
-      if (depObj._num < _num + 1) {
-         depObj._num = _num + 1;
-
-         if( sys.getPredecessorLists() ) {
-            SyncLockBlock lock( depObj._objectLock );
-            for ( DependableObjectVector::const_iterator it = depObj._predecessors.begin();
-                  it != depObj._predecessors.end(); it++ ) {
-               int value = (it->second->_lss == -1 ) ? depObj._num - 1 : (it->second->_lss < depObj._num - 1 ? depObj._num - 1 : it->second->_lss );
-               it->second->_lss = value;
-            }
-         }
-      }
-      if ( _lss == -1 ) {
-         _lss = depObj._num - 1;
-      } else if ( depObj._num < _lss ) {
-         _lss = depObj._num - 1;
-      }
-   }
-
-   //Maintain the list of predecessors
-   if ( sys.getPredecessorLists() ) {
-      SyncLockBlock lock( depObj._objectLock );
-      depObj.addPredecessor( *this );
-   }
-
-   sys.getDefaultSchedulePolicy()->atSuccessor( depObj, *this );
-
-   return _successors.insert ( std::make_pair( depObj.getWD() == NULL ? 0 : depObj.getWD()->getId(), &depObj ) ).second;
-}
--- src/core/dependableobject.hpp
+++ src/core/dependableobject.hpp
@@ -190,46 +190,41 @@ inline bool DependableObject::addPredecessor ( DependableObject &depObj )
    return inserted;
 }
 
-// inline bool DependableObject::addSuccessor ( DependableObject &depObj )
-// {
-//    // Avoiding create cycles in dependence graph
-//    if ( this == &depObj ) return false;
-// 
-//    //Maintain the list of predecessors
-//    if(sys.getPredecessorLists())
-//       depObj.addPredecessor( *this );
-// 
-//    sys.getDefaultSchedulePolicy()->atSuccessor( depObj, *this );
-// 
-//    return _successors.insert ( std::make_pair( depObj.getWD() == NULL ? 0 : depObj.getWD()->getId(), &depObj ) ).second;
-// }
-//inline bool DependableObject::addSuccessor ( DependableObject &depObj )
-//{
-//   // Avoiding create cycles in dependence graph
-//   if ( this == &depObj ) return false;
-//
-//   if (depObj._num < _num + 1) {
-//      depObj._num = _num + 1;
-//      if(sys.getPredecessorLists()) {
-//         std::cerr << "UPDATE LSS " << std::endl;
-//         for ( DependableObjectVector::const_iterator it = depObj._predecessors.begin();
-//               it != depObj._predecessors.end(); it++ ) {
-//            int value = ((*it)->_lss == -1 ) ? depObj._num - 1 : ( (*it)->_lss <= depObj._num - 1 ? (*it)->_lss : depObj._num - 1 );
-//            std::cerr << "set value " << value << " for wd " << (*it)->getWD()->getId() << std::endl;
-//            (*it)->_lss = value;
-//         }
-//      }
-//   }
-//
-//   //Maintain the list of predecessors
-//   if(sys.getPredecessorLists()) {
-//      std::cerr << "added predecessor " << depObj.addPredecessor( *this ) << std::endl;
-//   }
-//
-//   sys.getDefaultSchedulePolicy()->atSuccessor( depObj, *this );
-//
-//   return _successors.insert ( &depObj ).second;
-//}
+inline bool DependableObject::addSuccessor ( DependableObject &depObj )
+{
+   // Avoiding create cycles in dependence graph
+   if ( this == &depObj ) return false;
+
+   if ( sys._preSchedule ) {
+      if (depObj._num < _num + 1) {
+         depObj._num = _num + 1;
+
+         if ( sys.getPredecessorLists() ) {
+            SyncLockBlock lock( depObj._objectLock );
+            for ( DependableObjectVector::const_iterator it = depObj._predecessors.begin();
+                  it != depObj._predecessors.end(); it++ ) {
+               int value = (it->second->_lss == -1 ) ? depObj._num - 1 : (it->second->_lss < depObj._num - 1 ? depObj._num - 1 : it->second->_lss );
+               it->second->_lss = value;
+            }
+         }
+      }
+      if ( _lss == -1 ) {
+         _lss = depObj._num - 1;
+      } else if ( depObj._num < _lss ) {
+         _lss = depObj._num - 1;
+      }
+   }
+
+   //Maintain the list of predecessors
+   if ( sys.getPredecessorLists() ) {
+      SyncLockBlock lock( depObj._objectLock );
+      depObj.addPredecessor( *this );
+   }
+
+   sys.getDefaultSchedulePolicy()->atSuccessor( depObj, *this );
+
+   return _successors.insert ( std::make_pair( depObj.getWD() == NULL ? 0 : depObj.getWD()->getId(), &depObj ) ).second;
+}
 
 inline bool DependableObject::deleteSuccessor ( DependableObject *depObj )
 {
