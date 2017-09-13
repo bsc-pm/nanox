be41ada2cf4d1c293fda13d55d58df5fc2ee6eba
Jaume Bosch <jbosch@bsc.es>
2017-09-13 11:29:00 +0200

    Fix copyright year in sync_taskwait_mandatory test

--- tests/test/03_api/sync_taskwait_mandatory.c
+++ tests/test/03_api/sync_taskwait_mandatory.c
@@ -1,5 +1,5 @@
 /*************************************************************************************/
-/*      Copyright 2015 Barcelona Supercomputing Center                               */
+/*      Copyright 2017 Barcelona Supercomputing Center                               */
 /*                                                                                   */
 /*      This file is part of the NANOS++ library.                                    */
 /*                                                                                   */
