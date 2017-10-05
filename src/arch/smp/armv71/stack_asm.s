513a008a8fac3526eaf430926f0798664536e0f8
Jaume Bosch <jbosch@bsc.es>
2017-10-05 09:23:36 +0200

    Add padding in ULT stack creation for ARM
    In some applications, we found that some prints show wrong values but only when the ULT is enabled (which is the default). This seems to be due a different alignment of the variables in the stack, adding some padding before the first stack elements seems to fix the printf problem

--- src/arch/smp/armv71/stack_asm.s
+++ src/arch/smp/armv71/stack_asm.s
@@ -7,7 +7,7 @@
 switchStacks:
 
 	/* Saves general purpose registers,
-           R12 is included to make them even number */
+	      R12 is included to make them even number */
 	push {r4-r12,lr}
 
 	/* switch current sp to new */
@@ -15,11 +15,11 @@ switchStacks:
 	mov r2, sp
 	mov sp, r4
 
-	/* arguments in r0=arg1, r1=arg2, r2=old sp */
-	blx r3
+	/* arguments in r0=arg0, r1=arg1, r2=old sp */
+	   blx r3
 
 	/* Restores general purpose registers*/
-	pop {r4-r12,pc} 
+	pop {r4-r12,pc}
 
 startHelper:
 	pop {r0,r4}  /*User Args (R0) to Call user Function (R4)*/
@@ -27,4 +27,3 @@ startHelper:
 	pop {r0,r4}  /*Cleanup Args (R0) to Call cleanup function(R4)*/
 	blx r4       /*Jumps to cleanup*/
 	b abort     /*Aborts in case of cleanup returns*/
-
